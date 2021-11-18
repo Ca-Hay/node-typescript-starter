1e1291e787d6e708e63d26beaad22e554d504ff5
cameron <me@you.com>
2021-11-18 09:29:31 +0000

    adding a function to calculate the area of a circle

--- src/simpleMaths.ts
+++ src/simpleMaths.ts
@@ -1,8 +1,14 @@
+const pi = 3.14159265359
+
 export function calcAreaSquare(sideLength: number): number {
     return Math.pow(sideLength, 2);
 }
 
 export function calcAreaTriangle(width: number, height: number): number {
     return width * height / 2;
+}
+
+export function calcAreaCircle(radius: number): number {
+    return radius ** 2 * pi;
 }
 
